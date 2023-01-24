286a8f15d8d0fef8a87a223a06ae411a7e8e100a
Tatiana Inama <inama@prisma.io>
2023-01-24 11:32:01 +0100

    changes

--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,23 @@
 import { PrismaClient } from '@prisma/client'
+import useAccelerate from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(useAccelerate);
 
 const main = async () => {
   const albums = await prisma.album.findMany({
     where: {
       name: {
-        contains: ' ',
+        contains: 'a',
       },
     },
-  })
+    // cacheStrategy: {
+    //   swr: 60,
+    //   ttl: 10,
+    // },
+  });
 
-  console.log('Albums that have more than 1 word: ', albums)
-}
+  console.log('Albums that have more than 1 word: ', albums);
+};
 
 main()
   .catch((e) => console.error('Error in Prisma Client query: ', e))
